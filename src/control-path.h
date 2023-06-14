eb17cd0778dc58d1a6605d99c77d866218d245b6
Wanderson Soares <wandersonsoares841@gmail.com>
2023-06-14 16:13:50 -0300

    add regs p/ 32 bits

--- src/control-path.h
+++ src/control-path.h
@@ -6,21 +6,33 @@
 
 typedef unsigned char Byte;
 
-typedef Byte Word8;   // palavra de dados de 8 bits
+//  typedef Byte Word8;   // palavra de dados de 8 bits
 
-typedef Byte Word32[4];   // palavra de dados de 32 bits
+typedef Byte Word32[4];   // palavra de dados(de 32 bits) da memória
 
 // 5 bits p/ prox instrução + 3 jump + 8 p/ ALU + 9 p/ BUS_C + 3 p/ memória principal + 4 p/ BUS_B
 Word32 control_memory[32];
-
 Word32 MPC;
-
-//29 bits usáveis (- 3 bits p/ RAM)
-Word32 MIR;
+Word32 MIR;     //29 bits usáveis (- 3 bits p/ RAM)
 
 Word32 BUS_A;
 Word32 BUS_B;
 Word32 BUS_C;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #endif
