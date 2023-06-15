8ba61791ee30abe2b36b21a765b1be42d776f68f
Wanderson Soares <wandersonsoares841@gmail.com>
2023-06-15 16:56:27 -0300

    início da implementação da memória

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,6 +1,48 @@
-#include <control-path.h>
+#include <iostream>
+#include "../memory.hpp"
 
-// memória de 1 mbyte, cada palavra consistindo em 4 bytes
-Word32 main_memory[ 1024 * 1024 / 4 ] = {0};
+//std::array<Word32, 1024 * 1024 / 4> memory = {};
 
+Word MainMemory :: read_word(Word addr){
+    addr &= 0b111111111111111111; // 18 bits para representar os pinos de endereçamento (2^(18)= 262.144 )
+    return memory[addr];
+}
+
+void MainMemory :: write_word(Word addr, Word value){
+    addr &= 0b111111111111111111; // OBS
+    value &= 0xFFFFFFFF; // 8 F's
+    memory[addr] = value;
+}
+
+ // retorna o conteúdo do byte bt da memória principal
+ Byte MainMemory :: read_byte(unsigned long int bt){
+    bt &= 0b111111111111111111;     //*
+    Word word_addr = bt >> 2;           // bt / 4
+    Word val_word = memory[word_addr];
+
+    Byte bt_pos = bt & 0b11;         // resto da divisão por 4
+    Word val_byte = val_word >> (bt_pos << 3);  //desloca o valor do byte até a posição mais à esquerda
+    val_byte &= 0xFF;               //  zera o restante
+    return val_byte;
+}
+
+// escreve no byte "bt" da memória o byte value
+// 262.144 * 4 = 1048576(100000000000000000000) bytes possíveis para escrita
+void MainMemory :: write_byte(unsigned long int bt, Byte value){
+    bt &= 0b111111111111111111;
+    //value &= 0xFF;
+    Word word_addr = bt >> 2;           // bt / 4
+    Word word_val = memory[word_addr];
+
+    Byte bt_pos = bt & 0b11;         // resto da divisão por 4
+
+    // desloca 1 byte até a posição alvo na palavra onde bt está localizado
+    Word mask = 0xFF << (bt_pos << 3);  // 0xFF << (bt_pos * 8)
+    mask = ~mask;                       // deixa zerado o espaço do byte onde value será escrito
+    word_val &= mask;
+
+    value <<= (bt_pos << 3);    //  value <<= (bt_pos * 8);
+    memory[word_addr] = word_val | value;
+}
+    
 
