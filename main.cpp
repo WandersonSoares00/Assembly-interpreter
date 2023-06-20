d056a978d7907e31203e46ae2902e16e7c9cdcec
Wanderson Soares <wandersonsoares841@gmail.com>
2023-06-20 12:46:20 -0300

    testes

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include "src/inc/memory.hpp"
+#include "src/inc/cpu.hpp"
 
 using namespace std;
 
 int main(){
 	MainMemory men;
+	Cpu cpu;
 
-	//men.write_byte(103, 55);
-	men.write_byte(101, 78);
+	men.write_word(0, 12);
+	men.write_word(1, 37);
 
-	cout << bin<4>(men.memory[25]) << '\n';
-	//cout << men.read_word(25) << '\n';
-	cout << men.read_byte(101) << '\n';
+	//cpu.setFirmware();
+
+	cout << "Execução finalizada em: " << cpu.start() << '\n';
+
+	cout << men.read_word(3) << '\n';
 
 	return EXIT_SUCCESS;
 }
+
