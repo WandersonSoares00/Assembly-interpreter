7f34519703f562ff60235c4b45fa268b5bd26ee0
Wanderson Soares <wandersonsoares841@gmail.com>
2023-06-20 12:44:24 -0300

    exemplos de programação do firmware

---
+++ examples/ex1.hpp
@@ -0,0 +1,31 @@
+
+
+// Escreva um programa que soma o valor da posição de memória 0 com e 1 e escreva o resultado na posição 3
+
+/*
+    formato de microinstrução (de 32 bits):
+    próxima instrução: 9 bits
+    jump: 3 bits
+    operação na ula: 8 bits
+    barramento c: 6
+    operação de memória: 3 bits
+    barramento b: 3 bits
+
+    -------- Formato :  --------
+    long int* get_firmware (){
+        long int *firmware = new long int[512];
+
+        (...)   --sua implementação aqui
+
+        return firmware;
+    }
+*/
+
+
+long int* get_firmware (){
+        long int *firmware = new long int[512];
+
+        
+
+        return firmware;
+}
