744bae62adb635b3da2b3f3800670ae96eea5bc3
Wanderson Soares <wandersonsoares841@gmail.com>
2023-06-20 17:30:39 -0300

    adicionei a possibilidade de mudar o firmware

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "src/inc/memory.hpp"
 #include "src/inc/cpu.hpp"
+#include "examples/ex1.hpp"
 
 using namespace std;
 
@@ -11,11 +12,16 @@ int main(){
 	men.write_word(0, 12);
 	men.write_word(1, 37);
 
-	//cpu.setFirmware();
+	cpu.setMemory(&men);
+	Word *firmware = get_firmware();
+	cpu.setFirmware(get_firmware());
 
-	cout << "Execução finalizada em: " << cpu.start() << '\n';
+	int time = cpu.start();
+	cout << "Execução finalizada em: " << time << " passos\n";
 
 	cout << men.read_word(3) << '\n';
+
+	delete[] firmware;
 
 	return EXIT_SUCCESS;
 }
