5f7eca0c1fa6130cf0b994fbf8624da2dc4dee38
Wanderson Soares <wandersonsoares841@gmail.com>
2023-06-20 17:54:43 -0300

    exemplo de microprogramação

--- examples/ex1.hpp
+++ examples/ex1.hpp
@@ -26,14 +26,13 @@ unsigned long int* get_firmware (){
         unsigned long int *firmware = new unsigned long int[512];
 
         firmware[0] = 0b000000001'000'01110101'000100'010'011; // mdr = men[mar], bus_b = x, ++bus_b, bus_b<<1, x = bus_b (x = 2)
-        firmware[1] = 0b000000010'000'00000000'000000'000'000; // bus_b = mdr
-        firmware[2] = 0b000000011'000'00010100'000001'000'111; // bus_a = bus_b
+        firmware[1] = 0b000000010'000'00010100'000001'000'000; // bus_b = mdr, bus_a = bus_b
 
-        firmware[3] = 0b000000100'000'00110001'100000'010'111; // mar = 1, mdr = men[mar]
-        firmware[4] = 0b000000101'000'00110101'100000'000'011; // bus_b = x, ++bus_b, mar = bus_b (mar = 3)
-        firmware[5] = 0b000000110'000'00000000'000000'000'000; // bus_b = mdr
-
-        firmware[6] = 0b111111111'000'00111100'010000'100'000; // bus_c = bus_b(mdr) + bus_a, mdr = bus_c, men[3] = mdr, halt
+        firmware[2] = 0b000000011'000'00110001'100000'010'111; // mar = 1, mdr = men[mar]
+        firmware[3] = 0b000000100'000'00110101'100000'000'011; // bus_b = x, ++bus_b, mar = bus_b (mar = 3)
+        
+                                                               // bus_b = mdr, bus_c = bus_b(mdr) + bus_a
+        firmware[4] = 0b111111111'000'00111100'010000'100'000; // mdr = bus_c, men[3] = mdr, halt
         
         firmware[511] = 0;  // halt
 
