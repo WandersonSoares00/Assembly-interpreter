e29e44b93cae8253e7b2f8448d3787493619f902
Wanderson Soares <wandersonsoares841@gmail.com>
2023-06-20 17:28:35 -0300

    exemplo de programa com microprogramação

--- examples/ex1.hpp
+++ examples/ex1.hpp
@@ -7,13 +7,13 @@
     próxima instrução: 9 bits
     jump: 3 bits
     operação na ula: 8 bits
-    barramento c: 6
-    operação de memória: 3 bits
-    barramento b: 3 bits
+    barramento c: 6 (mar, mdr, pc, x, y, h)
+    operação de memória: 3 bits (wr, rd, ft) (wr: memory[mar] = mdr)
+    barramento b: 3 bits (0: mdr, 1: pc, 2: mbr, 3: x, 4: y, 5-7: nenhum)
 
     -------- Formato :  --------
-    long int* get_firmware (){
-        long int *firmware = new long int[512];
+    unsigned long int* get_firmware (){
+        long int *firmware = new unsigned long int[512];
 
         (...)   --sua implementação aqui
 
@@ -22,10 +22,23 @@
 */
 
 
-long int* get_firmware (){
-        long int *firmware = new long int[512];
+unsigned long int* get_firmware (){
+        unsigned long int *firmware = new unsigned long int[512];
 
+        firmware[0] = 0b000000001'000'01110101'000100'010'011; // mdr = men[mar], bus_b = x, ++bus_b, bus_b<<1, x = bus_b (x = 2)
+        firmware[1] = 0b000000010'000'00000000'000000'000'000; // bus_b = mdr
+        firmware[2] = 0b000000011'000'00010100'000001'000'111; // bus_a = bus_b
+
+        firmware[3] = 0b000000100'000'00110001'100000'010'111; // mar = 1, mdr = men[mar]
+        firmware[4] = 0b000000101'000'00110101'100000'000'011; // bus_b = x, ++bus_b, mar = bus_b (mar = 3)
+        firmware[5] = 0b000000110'000'00000000'000000'000'000; // bus_b = mdr
+
+        firmware[6] = 0b111111111'000'00111100'010000'100'000; // bus_c = bus_b(mdr) + bus_a, mdr = bus_c, men[3] = mdr
         
+        firmware[511] = 0;  // halt
 
         return firmware;
 }
+
+
+
