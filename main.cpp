09098bd24e9ea8213c76ccd14449ea7c7358cff9
Wanderson Soares <wandersonsoares841@gmail.com>
2023-06-23 21:05:59 -0300

    teste de microprogramação

--- main.cpp
+++ main.cpp
@@ -12,26 +12,42 @@ int main(){
 		Word *firmware = get_firmware();
 		cpu.setFirmware(firmware);
 	
+	//	(par: operação, operando)
+	
+	// salta para a instrução 5 caso X == 0
+	//men.write_byte(1, 11);
+	//men.write_byte(2, 5);
+
+	// soma men[50] + men[100]
+
 	men.write_word(50, 80);		//men[50] = 80
-	men.write_word(100, 15);	//men[100] = 15
+	men.write_word(100, 150);	//men[100] = 150
 
 	// x = x + men[50]
-	men.write_byte(1, 2);	// operando 2: soma
-	men.write_byte(2, 50);	// 50 é o endereço de men[50]
+	//men.write_byte(1, 2);	// operando 2: soma
+	//men.write_byte(2, 50);	// 50 é o endereço de men[50]
+	
 
 	// x = x + men[100]
 	men.write_byte(3, 2);
 	men.write_byte(4, 100);
 
+	// x = x - men[100]
+	men.write_byte(5, 13);
+	men.write_byte(6, 50);
+
 	//cout << bin <8> (men.read_byte(4)) << '\n';
-	
+
+	men.write_byte(7, 6);	// chama o operando para armazenar X em men[150]
+	men.write_byte(8, 150);
+
+	men.write_byte(9, 255);	// halt
+
 	int time = cpu.start(true);
 	cout << "Execução finalizada em: " << time << " passos\n";
 
-	cout << cpu.registers.X << '\n';
-
-	cpu.imprime_regs(); 		// binario
-	cpu.imprime_regs(false);	// decimal
+	cout << men.read_word(150) << '\n';
+	//cout << cpu.registers.X << '\n';
 
 	delete[] firmware;
 
