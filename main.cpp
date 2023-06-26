a3901f313e95528a65be98220ef78be5e0d1aea5
Wanderson Soares <wandersonsoares841@gmail.com>
2023-06-25 21:54:12 -0300

    programa teste

--- main.cpp
+++ main.cpp
@@ -20,8 +20,8 @@ int main(){
 
 	// soma men[50] + men[100]
 
-	men.write_word(50, 80);		//men[50] = 80
-	men.write_word(100, 150);	//men[100] = 150
+	men.write_word(2, 80);		//men[50] = 80
+	men.write_word(3, 150);	//men[100] = 150
 
 	// x = x + men[50]
 	//men.write_byte(1, 2);	// operando 2: soma
@@ -29,24 +29,24 @@ int main(){
 	
 
 	// x = x + men[100]
-	men.write_byte(3, 2);
-	men.write_byte(4, 100);
+	men.write_byte(1, 2);
+	men.write_byte(2, 2);
 
 	// x = x - men[100]
-	men.write_byte(5, 13);
-	men.write_byte(6, 50);
+	men.write_byte(3, 13);
+	men.write_byte(4, 3);
 
 	//cout << bin <8> (men.read_byte(4)) << '\n';
 
-	men.write_byte(7, 6);	// chama o operando para armazenar X em men[150]
-	men.write_byte(8, 150);
+	men.write_byte(5, 6);	// chama o operando para armazenar X em men[150]
+	men.write_byte(6, 3);
 
-	men.write_byte(9, 255);	// halt
+	men.write_byte(7, 255);	// halt
 
-	int time = cpu.start(true);
+	int time = cpu.start();
 	cout << "Execução finalizada em: " << time << " passos\n";
 
-	cout << men.read_word(150) << '\n';
+	cout << men.read_word(3) << '\n';
 	//cout << cpu.registers.X << '\n';
 
 	delete[] firmware;
