ad78192b002e00dfc811b9917c9faa4326a7618c
gaabirel <gabrieljogos400@gmail.com>
2023-06-29 12:39:52 -0300

    divisao.asm adicionado

---
+++ examples/divisao.asm
@@ -0,0 +1,27 @@
+; 2. Um programa que calcula a divisão inteira, retornando como resultado o valor da divisão e o resto
+goto main
+wb 0
+
+i   ww 0        ; divisao inteira
+r   ww 0        ; resto da divisão
+a   ww 135
+b   ww 78
+one ww 1
+
+main:
+    add x, b    ; x+=b
+    div x, a    ; x = a/x
+    mov x, i    ; i=x   ; divisao inteira
+    
+    sub x, i    ; x=0
+    add x, b    ; x=b
+
+    mul x, i    ; x *= i
+    mov x, b    ; b=x
+    sub x, b    ; x=0
+    add x, a    ; x+=a
+    sub x, b    ; x-=b
+
+    mov x, r    ; r=x
+
+    halt        ; encerra o programa
