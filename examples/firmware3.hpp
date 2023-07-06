811fd6cd1cd1322de4e32f6bf83fc3d0633ddd9c
Wanderson Soares <wandersonsoares841@gmail.com>
2023-07-06 09:04:53 -0300

    ⠀⠀⠀⠀⠀

--- examples/firmware3.hpp
+++ examples/firmware3.hpp
@@ -12,16 +12,19 @@
     * 7) X = X * men[address]
     * 8) X = X / men[address]
     * 9) X = mem[address]
-    * 10) Y = Y - mem[address]
-    * 11) Y = Y * men[address]
-    * 12) Y = Y / men[address]
-    * 13) Y = mem[address]
-    * 14) X = X - Y
-    * 15) X = X + Y
-    * 16) X = X * Y
-    * 17) X = X / Y
-    * 18) halt
+    * 10) if Y = 0 then goto address
+    * 11) Y = Y + mem[address]
+    * 12) Y = Y - mem[address]
+    * 13) Y = Y * men[address]
+    * 14) Y = Y / men[address]
+    * 15) Y = mem[address]
+    * 16) X = X + Y
+    * 17) X = X - Y
+    * 18) X = X * Y
+    * 19) X = X / Y
+    * 20) halt
 */
+/*   Formato de microinstrução: prox_inst(9)'jamp(3)'ula(8)'bus_c(6)'memória(3)'ifu(3)'bus_a(3)'bus_b(3)*/
 
 long int* get_firmware (){
         long int *firmware = new long int[512];
@@ -49,40 +52,37 @@ long int* get_firmware (){
         firmware[9]  =  0b000000000'100'00010100'000100'100'111'000'000;
 
 
-        /*      Instrução 2  ->  Y = Y + mem[address]   */
+        /*      Instrução 11  ->  Y = Y + mem[address]   */
         firmware[18] =   0b000000000'100'00111100'000010'000'111'000'100;
-        /*      Instrução 3    ->  mem[address] = Y  */
+        /*      Instrução 1    ->  mem[address] = Y  */
         firmware[19]  =  0b000000000'100'00010100'010000'100'111'000'100;
-        /*      Instrução 5   ->  if Y = 0 then goto address */
-        // 20: X <- X; IF ALU = 0 GOTO 277 (256 à frente) ELSE GOTO 21
-        // 21: PC <- PC + 1; PC <- PC + 1; fetch; Goto mbr
-        // 277: GOTO 100
+        /*      Instrução 10   ->  if Y = 0 then goto address */
         firmware[20]  =  0b000010101'001'00010100'000010'000'000'000'100;
         firmware[21]  =  0b000000000'100'00000000'000000'000'101'000'000;
 
         /*      Instrução 4    ->  goto address    */
-        firmware[149]  =  0b000000000'100'00010100'001000'001'001'000'010;  //aqui
+        firmware[149]  =  0b000000000'100'00010100'001000'001'001'000'010;
 
-        /*      Instrução 6  ->  Y = Y - mem[address]    */
+        /*      Instrução 12  ->  Y = Y - mem[address]    */
         firmware[22]  =  0b000000000'100'00111111'000010'000'111'000'100;
-        /*      Instrução 7  ->  Y = Y * mem[address]    */
+        /*      Instrução 13  ->  Y = Y * mem[address]    */
         firmware[23]  =  0b000000000'100'00001100'000010'000'111'000'100;
-        /*      Instrução 8  ->  Y = Y / mem[address]    */
+        /*      Instrução 14  ->  Y = Y / mem[address]    */
         firmware[24]  =  0b000000000'100'00011100'000010'000'111'000'100;
-        /*      Instrução 9  ->  Y = mem[address]    */
+        /*      Instrução 15  ->  Y = mem[address]    */
         firmware[25]  =  0b000000000'100'00010100'000010'100'111'000'000;
 
 
-        /*      Instrução 2  ->  X = X + Y   */
+        /*      Instrução 16  ->  X = X + Y   */
         firmware[34] =   0b000000000'100'00111100'000100'000'001'100'011;
-        /*      Instrução 6  ->  X = X - Y    */
+        /*      Instrução 17  ->  X = X - Y    */
         firmware[35]  =  0b000000000'100'00111111'000100'000'001'100'011;
-        /*      Instrução 7  ->  X = X * Y    */
+        /*      Instrução 18  ->  X = X * Y    */
         firmware[36]  =  0b000000000'100'00001100'000100'000'001'100'011;
-        /*      Instrução 8  ->  X = X / Y    */
+        /*      Instrução 19  ->  X = X / Y    */
         firmware[37]  =  0b000000000'100'00011100'000100'000'001'100'011;
 
-        /*    Instrução 10   -> halt      */
+        /*    Instrução 20   -> halt      */
         firmware[255] = 0;
 
         return firmware;
